b7bf0cf50216db6e7d61213bd9c44237c4e69f35
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2022-06-21 20:04:06 +0200

    Add rock paper scissors logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,39 @@
 const score = document.getElementById('score')
-console.log(score)
+const result = document.getElementById('result')
 
-function pick(weapon) {
-  console.log(weapon)
+const choices = {
+  rock: {
+    wins: ['scissors'],
+    loses: ['paper'],
+  },
+  paper: {
+    wins: ['rock'],
+    loses: ['scissors'],
+  },
+  scissors: {
+    wins: ['paper'],
+    loses: ['rock'],
+  }
+}
+
+let userScore = 0
+let computerScore = 0
+
+function pick(userPick) {
+  // Choose random weapon from choices
+  const choicesArray = Object.keys(choices)
+  const randomChoice = Math.floor(Math.random() * choicesArray.length)
+  const computerPick = choicesArray[randomChoice]
+
+  const computer = choices[computerPick]
+  if (computer.wins.includes(userPick)) {
+    computerScore++
+    result.innerText = `Computer picks ${computerPick}. You lose!`
+  } else if (computer.loses.includes(userPick)) {
+    userScore++
+    result.innerText = `Computer picks ${computerPick}. You win!`
+  } else {
+    result.innerText = `Computer picks ${computerPick}. It's a draw!`
+  }
+  score.innerText = `${userScore}:${computerScore}`
 }
